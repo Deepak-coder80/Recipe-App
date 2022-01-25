f44aa93a85bf6bf3fdf0d1747718a31eef4b5017
Deepak M S <souparnikad40@gmail.com>
2022-01-25 12:17:38 +0530

    deltails  page ingredents added

--- lib/screens/recipe_detail.dart
+++ lib/screens/recipe_detail.dart
@@ -33,6 +33,17 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.lable,
               style: const TextStyle(fontSize: 18),
             ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text(
+                      '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                },
+              ),
+            ),
           ],
         ),
       ),
